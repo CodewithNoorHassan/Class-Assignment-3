c6414c8c264cd3b632d327fa100724941a75bafc
Noor Hassan <noornh635@gmail.com>
2024-10-25 21:05:57 +0500

    a
    GIAIC ASSIGNMENT OF QUARTER-2

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+export const metadata = {
+  title: "Home"
+}
+export default function Home() {
+  return (
+    <><div className="flex mt-4 px-4">
+      <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl mx-1">
+        <h3 className="text-slate-900 dark:text-white text-base font-bold tracking-tight">Certified Cloud Applied Generative AI Engineer (GenEng) and Solopreneur Developing Billion-Dollar Valued Developers and Solopreneurs</h3>
+        <p className="text-slate-500 dark:text-slate-400 mt-1 text-sm">
+          The pace of technological change is accelerating, big players like Microsoft, Amazon, Google, and OpenAI are winning by providing infrastructure, large AI foundation models, frameworks, 3D Metaverse experiences, and massive distribution networks. Solopreneurs trained in this program will win by automating work typically outsourced to employees, by directly connecting to customers by eliminating the middleman, and by developing vertical metaverses, thus paving the way for the first billion-dollar valued solopreneur businesses. This program has the objective to train this new breed of billion-dollar solopreneurs. These solopreneurs will adopt the ultra-lean business model and work independently and will not need to hire employees or other team members.
+        </p>
+      </div>
+    </div>
+</>
+  )
+}
